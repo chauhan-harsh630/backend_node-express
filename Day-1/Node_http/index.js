b2e50901c5ea0d0b6e36e525c953259b3d9910f0
Harsh Chauhan <hc2668012@gmail.com>
2026-02-01 19:35:51 +0530

    refactor: http routs in node

--- Day-1/Node_http/index.js
+++ Day-1/Node_http/index.js
@@ -1,12 +1,43 @@
-import http from "http"; 
-const port = 3000
-const app = http.createServer((req, res) => {
-    //    res.setHeader('Content-type',"text/html")
-    res.writeHead(200,{'content-type':'application/json'})
-    // res.end("<h1>Hello World!</h1>");
-    res.end(JSON.stringify({ message: "Server is running" }));
+import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
+
+const port = 3000;
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = http.createServer(async (req, res) => {
+
+    if (req.method !== "GET") {
+        res.writeHead(405, { "Content-Type": "text/plain" });
+        return res.end("Method Not Allowed");
+    }
+
+    let filePath;
+    
+    if (req.url === "/") {
+        filePath = path.join(__dirname, "public", "home.html");
+    } 
+    else if (req.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+    } 
+    else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        return res.end("Page Not Found");
+    }
+
+    try {
+        const data = await fs.readFile(filePath);
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    } catch {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+    }
 });
 
 app.listen(port, () => {
-    console.log(`Server running to port: http://localhost:${port}`);
-})
+    console.log(`Server running at http://localhost:${port}`);
+});
